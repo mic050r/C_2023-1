979f1563eed39c3cc1c188ae4e08b1f54c4d6601
mic050r <s2216@e-mirim.hs.kr>
2023-03-17 09:50:38 +0900

    변수 출력하기

--- Project1/문법/C++ 기본.cpp
+++ Project1/문법/C++ 기본.cpp
@@ -1,10 +1,13 @@
 ﻿// cout 객체 사용
 #include <iostream>
-
+// 이름공간(변수 이름의 중복 방지 std)
+using namespace std;
+// 
 int main(void) {
+	int year = 2023;
 	// << 출력 연산자
 	// endl : 개행
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << year << endl;
 	
 	return 0;
 }
