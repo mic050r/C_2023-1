d03ad717084223810078a287a80c7bd21eb3823a
mic050r <s2216@e-mirim.hs.kr>
2023-03-24 09:44:39 +0900

    C 스타일 문자열 길이

---
+++ Project1/문법/문자열 길이.cpp
@@ -0,0 +1,18 @@
+﻿// C
+#include <stdio.h>
+#include <string>
+
+// C++
+#include <iostream>
+#include <string>	
+
+int main(void) {
+
+	char str1[30] = "Jihyeon";
+	int len = 0;
+
+	for (int i = 0; str1[i] != '\0'; i++) {
+		len++;
+	}
+	printf("문자열의 길이는 %d \n", len);
+}
