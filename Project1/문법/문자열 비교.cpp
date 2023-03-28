7971ee7a195f705f32aa8d8f0233aa5e9e523814
mic050r <s2216@e-mirim.hs.kr>
2023-03-28 14:58:35 +0900

    C++ 스타일 문자열 비교 에러 수정

--- Project1/문법/문자열 비교.cpp
+++ Project1/문법/문자열 비교.cpp
@@ -12,16 +12,28 @@ int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	
+	// 문자열 비교 (인자의 순서와 반환값이 헷갈려)
+	int result = strcmp(str1, str2);
+	if (result == 0) {
+		printf("str1 == str2");
+	}
 	if (str1 == str2) {
 		cout << "str1 == str2";
 	}
-	// str2가 str1보다 사전에 늦게 나오는 경우
-	else if(str1 < str2) {
+	else if (result == -1) {
+		printf("str1 < str2");
+		// str2가 str1보다 사전에 늦게 나오는 경우
+	}
+	else if (str1 < str2) {
 		cout << "str1 < str2";
 	}
+	else if (result == 1) {
+		printf("str1 > str2");
+	} 
 	else {
 		cout << "str1 > str2";
 	}
 	return 0;
 }
+		
+
