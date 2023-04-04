383c03dde67c77ca040d34997672c3e0bd6d0a91
mic050r <s2216@e-mirim.hs.kr>
2023-04-04 14:31:03 +0900

    매개변수가 있는 생성자

--- Project1/문법/클래스 기본.cpp
+++ Project1/문법/클래스 기본.cpp
@@ -17,14 +17,19 @@ public:
 		address = "경기도 수원시";
 	}
 
+public:
+	Student(int _hakbun, string _name, string _department, string _tel, string _address) {
+
+		hakbun = _hakbun;
+		name = _name;
+		department = _department;
+		tel = _tel;
+		address = _address;
+
+	}
+
 	//class 멤버 함수를 가질 수 있다
 	void print() {
-		hakbun = 2214;
-		name = "임지현";
-		department = "소프트웨어과";
-		tel = "010-2222-9999";
-		address = "경기도 수원시";
-
 		cout << "==========학생 정보==========" << endl;
 		cout << "학번 : " << hakbun << endl;
 		cout << "이름 : " << name << endl;
@@ -34,7 +39,12 @@ public:
 	}
 
 
+
 private:
+	// 성능 때문에 hakbun을 문자열로 하지 않음
+	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
+	// 정수는 비교 연산을 한 번에 할 수 있으나, 문자열을 글자수 만큼 반복하여 비교해야함
+	// class는 디폴트 private(struct는 디폴트가 public)
 
 	int hakbun;
 	string name;
@@ -46,15 +56,14 @@ private:
 
 int main(void) {
 	// 자료형 : Student(class 생략 가능)
-	class Student stu1;
-	// 성능 때문에 hakbun을 문자열로 하지 않음
-	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
-	// 정수는 비교 연산을 한 번에 할 수 있으나, 문자열을 글자수 만큼 반복하여 비교해야함
-	// class는 디폴트 private(struct는 디폴트가 public)
-
-
-	stu1.print();
-
+	
+	
+	// 매개변수가 없는 생성자
+	Student stu1 = Student();
+	// Student stu1;
+	//stu1.print();
+	Student stu2 = Student(2214, "최연준와이프", "010-1004-1004", "뉴미디어소프트웨어과", "네마음속");
+	stu2.print();
 	return 0;
 }
 
