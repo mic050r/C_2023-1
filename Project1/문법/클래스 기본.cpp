5adb9063ac741849a3a17d01ad6759296e7d796c
mic050r <s2216@e-mirim.hs.kr>
2023-04-04 13:42:37 +0900

    생성자란?

--- Project1/문법/클래스 기본.cpp
+++ Project1/문법/클래스 기본.cpp
@@ -12,6 +12,10 @@ public:
 	string department;
 	string address;
 
+	// 생성자 (constructor) : 객체를 생성할 때 호출되는 함수
+	// 생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의 된다
+	// Student(){}
+
 	//class 멤버 함수를 가질 수 있다
 	void print() {
 		cout << "==========학생 정보==========" << endl;
