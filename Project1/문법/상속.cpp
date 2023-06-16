95839b38857f8342a586fac7e5445b12210fce24
mic050r <s2216@e-mirim.hs.kr>
2023-06-16 09:55:02 +0900

    소멸자에는 반드시 virtual을 넣자

--- Project1/문법/상속.cpp
+++ Project1/문법/상속.cpp
@@ -8,12 +8,14 @@ public:
 	{
 		cout << "동물 생성자" << endl; 
 	}
-	~Animal() { cout << "동물 소멸자" << endl; }
+	// 소멸에는 무조건 virtual을 넣자(메모리 누수 방비)
+	virtual ~Animal() { cout << "동물 소멸자" << endl; }
 
 	// 동적 바인딩(가상함수)
-	virtual void Bark(void) { cout << "동물 짖는다." << endl; }
-	virtual void Eat(void) { cout << "동물 먹는다" << endl; }
-	virtual void Hunt(void) { cout << "동물 사냥한다." << endl; }
+	// 동물은 추상적인 존재이므로 구체직인 구현 (함수)은 자식클래스에서 (순수가상함수)
+	virtual void Bark(void) = 0;
+	virtual void Eat(void) = 0;
+	virtual void Hunt(void) = 0;
 
 private:
 	int age_; 
@@ -29,12 +31,14 @@ public:
 		cout << "두루미 생성자" << endl; 
 	}
 
-	~Crane() { cout << "두루미 소멸자" << endl; }
+	virtual ~Crane() { cout << "두루미 소멸자" << endl; }
 	// 오타값은 실수를 방지하기 위해 override 키워드를 사용(Java의 @ovveride)
 	void Bark() override 
 	{
 		cout << "두루두루" << endl;
 	}
+	// 순수가상함수(추상메소드)는 반드시 자식에서 구현해야 한다.
+	void Eat() override{}
 private: 
 	int leg_length_;
 };
