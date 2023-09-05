861f60fcb238e5d29cb9799c63c5a9931c38fc24
mic050r <s2216@e-mirim.hs.kr>
2023-09-05 13:40:08 +0900

    템플릿 적용 전

--- Project1/문법/템플릿 클래스.cpp
+++ Project1/문법/템플릿 클래스.cpp
@@ -3,27 +3,13 @@
 
 using namespace std;
 
-template <typename T>
-class MyData {
-public:
-	MyData(T data) : data_(data) {}
-
-	T get_data(void) { return data_; }
-	void set_data(T data) { data_ = data; }
-private:
-	T data_;
-};
+int sum(int a, int b) { return a + b; }
+float sum(float a, float b) { return a + b; }
+string sum(string a, string b) { return a + b; }
 
 int main(void)
 {
-	MyData<int> m1 = MyData<int>(1);
-	cout << m1.get_data() << endl;
-	m1.set_data(3);
-	cout << m1.get_data() << endl;
-
-
-	MyData<string> m2 = MyData<string>("¿À´Ã ¹¹¸ÔÁö?");
-	cout << m2.get_data() << endl;
-	m2.set_data("¸¶¶ó¼§±Å!");
-	cout << m2.get_data() << endl;
+	cout << sum(1, 2) << endl;
+	cout << sum(1.f, 2.f) << endl;
+	cout << sum("1", "2") << endl;
 }
