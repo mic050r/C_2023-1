c5aa7f9f1488fa871bdf1bddff82829579068095
mic050r <s2216@e-mirim.hs.kr>
2023-09-05 13:46:00 +0900

    헤더파일 분할

--- FileSplit/main.cpp
+++ FileSplit/main.cpp
@@ -1,1 +1,51 @@
-﻿파일분할 진행 전
+﻿#include "animal.h"
+#include "cat.h"
+#include "dog.h"
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	void bark(void)
+	{
+
+	}
+private:
+	int leg_num_;
+};
+
+
+class Dog : public Animal {
+public:
+	void bark(void)
+	{
+		cout << "¸Û¸Û" << endl;
+	}
+private:
+	int speed;
+};
+
+
+class Cat : public Animal {
+public:
+	void bark(void)
+	{
+		cout << "¾ß¿Ë" << endl;
+	}
+private:
+	int claw;
+};
+
+int main(void)
+{
+	Animal a;
+	Dog poppi;
+	Cat nabi;
+
+	a.bark();
+	poppi.bark();
+	nabi.bark();
+
+	return 0;
+}
